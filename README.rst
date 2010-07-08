d46f48f6c4d6156d60b83ced333791613e078387
Dave Dash <dd+github@davedash.com>
2010-07-08 09:23:58 -0700

    setup.py, et al

---
+++ README.rst
@@ -0,0 +1,2 @@
+This is a memory intensive app that stores documents in a corpus and uses an
+inverted index to group objects together.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name='textcluster',
+        version='0.0.1',
+        description='Tool to cluster documents according to text similarity.',
+        long_description=open('README.rst').read(),
+        author='Dave Dash',
+        author_email='dd+pypi@davedash.com',
+        url='http://github.com/davedash/textcluster',
+        license='BSD',
+        packages=find_packages(),
+        include_package_data=True,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Text Processing :: Indexing',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
+
